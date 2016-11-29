81204b028498b8132fc28ef1a75d399fecaff48d
Charlotte Tortorella <charlotte@monadic.consulting>
2016-11-29 18:48:58 +1100

    Add a self reference variable to ensure that an observable does not deallocate while being observed

--- ReSwiftRx/Rx/ObservableProperty.swift
+++ ReSwiftRx/Rx/ObservableProperty.swift
@@ -11,6 +11,7 @@ public final class ObservableProperty<ValueType>: ObservablePropertyType {
         ObservablePropertySubscriptionReference<ValueType>
     internal var subscriptions = [ObservablePropertySubscriptionReferenceType : (ValueType) -> ()]()
     private var subscriptionToken: Int = 0
+    private var retainReference: ObservableProperty<ValueType>?
     public var value: ValueType {
         didSet {
             subscriptions.forEach { $0.value(value) }
@@ -24,7 +25,8 @@ public final class ObservableProperty<ValueType>: ObservablePropertyType {
     @discardableResult
     public func subscribe(_ function: @escaping (ValueType) -> Void) -> SubscriptionReferenceType? {
         defer { subscriptionToken += 1 }
-        defer { function(value) }
+        retainReference = self
+        function(value)
         let reference = ObservablePropertySubscriptionReferenceType(key: String(subscriptionToken),
                                                             stream: self)
         subscriptions.updateValue(function, forKey: reference)
@@ -33,5 +35,8 @@ public final class ObservableProperty<ValueType>: ObservablePropertyType {
 
     internal func unsubscribe(reference: ObservablePropertySubscriptionReferenceType) {
         subscriptions.removeValue(forKey: reference)
+        if subscriptions.count == 0 {
+            retainReference = nil
+        }
     }
 }
