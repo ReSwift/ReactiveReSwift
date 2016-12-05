4bebb4cded170a379382d155bed44cb4f8eda902
Charlotte Tortorella <charlotte@monadic.consulting>
2016-12-05 16:31:52 +1100

    Update documentation

--- ReactiveReSwift/CoreTypes/Store.swift
+++ ReactiveReSwift/CoreTypes/Store.swift
@@ -11,9 +11,8 @@ import Foundation
 /**
  This class is the default implementation of the `Store` protocol. You will use this
  store in most of your applications. You shouldn't need to implement your own store.
- You initialize the store with a reducer and an initial application state. If your app has multiple
- reducers you can combine them by initializng a `CombinedReducer` with all of your reducers as
- arguments.
+ You initialize the store with a reducer and an initial application state,
+ contained within an observable.
  */
 
 public class Store<ObservableProperty: ObservablePropertyType>: StoreType where ObservableProperty.ValueType: StateType {
