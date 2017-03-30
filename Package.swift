e38f5ff42496ec38a941dea1af8c8a99ddb9a34b
Charlotte Tortorella <charlotte.tortorella@icloud.com>
2017-03-30 20:54:41 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "ReSwiftRx",
+    name: "ReactiveReSwift",
     exclude: [
-      "ReSwiftRxTests",
+      "Tests",
       "Carthage",
       "Docs"
     ]
